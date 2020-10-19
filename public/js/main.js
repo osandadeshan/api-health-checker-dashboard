924cf3b023d2f79a136a7b66d0aeca459b646ffa
Osanda Deshan <osanda.deshan@gmail.com>
2020-10-19 22:01:46 +0530

    Implemented auto page refresh in every 60 seconds.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: OnePage - v2.1.0
-* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
